aaaa144a125de5cde83739b98be9d05f262a5771
Wjx <3244134327@qq.com>
2026-07-15 10:41:56 +0800

    feat: add miniprogram handling center

---
+++ miniprogram/pages/handling/index.js
@@ -0,0 +1,61 @@
+const dashboard = require('../../services/dashboard')
+const { normalizeUser } = require('../../adapters/auth')
+const { handlingEntries, normalizePendingSummary } = require('../../adapters/handling')
+const { can } = require('../../utils/permission')
+const { syncTabBar } = require('../../utils/tab-bar')
+
+Page({
+  data: {
+    user: {},
+    entries: [],
+    pendingSummary: normalizePendingSummary(null),
+    summaryLoading: false,
+    summaryError: ''
+  },
+
+  onLoad() {
+    const user = this.refreshEntries()
+    if (can(user, 'approval:view')) return this.loadSummary()
+  },
+
+  onShow() {
+    syncTabBar(this, 'handling')
+    this.refreshEntries()
+  },
+
+  refreshEntries() {
+    const user = normalizeUser(getApp().globalData.user)
+    this.setData({ user, entries: handlingEntries(user) })
+    return user
+  },
+
+  async loadSummary() {
+    if (this.data.summaryLoading || !can(this.data.user, 'approval:view')) return
+    this.setData({ summaryLoading: true, summaryError: '' })
+    try {
+      const raw = await dashboard.overview()
+      this.setData({ pendingSummary: normalizePendingSummary(raw) })
+    } catch (error) {
+      this.setData({
+        pendingSummary: normalizePendingSummary(null),
+        summaryError: '待审批数据暂不可用，请稍后重试'
+      })
+    } finally {
+      this.setData({ summaryLoading: false })
+      wx.stopPullDownRefresh()
+    }
+  },
+
+  onPullDownRefresh() {
+    if (!can(this.data.user, 'approval:view')) {
+      wx.stopPullDownRefresh()
+      return
+    }
+    return this.loadSummary()
+  },
+
+  open(event) {
+    const url = event.currentTarget.dataset.url
+    if (url) wx.navigateTo({ url })
+  }
+})
